67c1af1925c18587ea2a2f8056aae8f28d5b1a03
VibhorGits <vibhor.bhatt1705@gmail.com>
2023-05-05 17:24:02 +0530

    Integrated messaging with twilio

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
+from twilio.rest import Client
 
 open_weather_endpoint = "https://api.openweathermap.org/data/2.8/onecall"
+api_key = "5269c17b1ac2399afab64422d37d2e39"
+account_sid = "AC0e3d9ddc66af5411934c7b3f8d46b7c0"
+auth_token = "bfa47fbf2792bec8a6a1fcc943d1e725"
 
 parameters = {
     "lat": 30.316496,
     "lon": 78.032188,
-    "appid": "5269c17b1ac2399afab64422d37d2e39",
+    "appid": api_key,
     "exclude": "current,minutely,daily,alerts"
 }
 
@@ -13,14 +17,19 @@ response = requests.get(url=open_weather_endpoint, params=parameters)
 response.raise_for_status()
 weather_data = response.json()
 weather_slice = weather_data["hourly"][:12]
-print(weather_slice)
 rain = False
 
 for hourly_data in weather_slice:
     rain_code = hourly_data["weather"][0]["id"]
-    print(rain_code)
     if rain_code < 700:
         rain = True
 
 if rain:
-    print("Bring an Umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="+13204131570",
+        to="Your number with country code"
+    )
+    print(message.status)
